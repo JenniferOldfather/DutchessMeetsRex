578f02b43674d8d3c9cd74feaf388c046621a0f2
JenniferOldfather <JenniferOldfather@users.noreply.github.com>
2020-08-09 14:41:24 -0500

    Updated the app to bring in new drawables, updated the app to landscape only, added onClickListener for play button to bring us to the next activity which holds the play area background.

--- app/src/main/java/com/example/dutchessmeetsrex/MainActivity.kt
+++ app/src/main/java/com/example/dutchessmeetsrex/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.dutchessmeetsrex
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.dutchessmeetsrex.R.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var playButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
+        playButton = findViewById(id.playButton)
+        playButton.setOnClickListener{view: View ->
+            //create a new activity for the button
+            setContentView(layout.play_area_background)
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+        }
     }
 }
